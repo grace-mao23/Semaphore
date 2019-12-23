5981169ba1a1076490c70f60efd7327a6e349ad0
grace-mao23 <gmao00@stuy.edu>
2019-12-23 11:33:24 -0500

    Attempted write

--- write.c
+++ write.c
@@ -1,6 +1,44 @@
 #include "semaphore.h"
 
-int main() {
+int shmd, semd, fd;
+struct sembuf sb;
 
+int main() {
+  sb.sem_num = 0;
+  sb.sem_op = -1;
+  go_write();
+  return 0;
+}
+
+int go_write() {
+  printf("Trying to get in\n");
+  semd = semget(KEY, 1, 0);
+  if (semd < 0) {
+    printf("Error: %s\n", strerror(errno));
+    return -1;
+  }
+  semop(semd, &sb, 1);
+  shmd = shmget(KEY, sizeof(char *), 0);
+  if (shmd < 0) {
+    printf("Error: %s\n", strerror(errno));
+    return -1;
+  }
+  fd = open("telephone.txt", O_RDWR, 0644);
+  char *last = shmat(shmd, 0, 0);
+  printf("Last addition: %s\n\n", last);
+  char next[SIZE];
+  printf("Your addition: ");
+  fgets(input, SIZE, stdin);
+  int i = 0;
+  for (; i < sizeof(input); i++) {
+    if (input[i] == '\n') {
+      input[i] = '\0';
+    }
+  }
+  strcpy(last, input);
+  shmdt(last);
+  close(fd);
+  sb.sem_op = 1;
+  semop(semd, &sb, 1);
   return 0;
 }
